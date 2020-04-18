2aa765f2b9a7305e0cadbe68442e784bff54e8bb
For_fluke <flukeza09@hotmail.com>
2020-04-18 21:56:00 +0700

    ทำะ้นกห

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 new Vue({
     el:"#vue_app",
     data: {
-        message : "hello world"
+        message : "hello world",
+        name : "Tawin "
+    },
+    methods:{
+        getName:function(){
+            return this.name;
+        },
+        setName:function(n){
+            return this.name = n;
+        }
     }
 })
