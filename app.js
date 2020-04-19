149be76f8f7be86ac2622333260892acd1a343ef
For_fluke <flukeza09@hotmail.com>
2020-04-19 19:22:37 +0700

    methods;

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 new Vue({
     el:"#vue_app",
     data: {
-        like:false,
-        array_product_list:['product1','product2','product3','product4','product5','product6','product7'],
-        employee:[
-            {
-                name:'A',age:20,status:false
-            },{
-                name:'B',age:25,status:false
-            },{
-                name:'C',age:30,status:true
-            },{
-                name:'D',age:35,status:true
-            },{
-                name:'E',age:40,status:true
-            },
-        ]
+      newData:{
+          empName:'',
+          salary: 0
+      },
+      empGroup:[
+        {
+            empName:  'mr.A',
+            salary: 5000
+        },
+        {
+            empName:  'mr.B',
+            salary: 6000
+        }
+      ]
     },
     methods:{
-        eventChange:function(){
-            if( this.like == true){
-                this.like = false
-            }else{
-                this.like = true
-            }
+        appEmp:function(){
+            this.empGroup.push(
+                {
+                    empName:this.newData.empName,
+                    salary : this.newData.salary
+                }
+              
+            );
+            this.newData.empName = '';
+            this.newData.salary = 0;
         }
     }
 })
