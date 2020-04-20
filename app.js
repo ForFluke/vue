ca80a643ed20ba0ffe06f435e78beb6d864ecfa7
For_fluke <flukeza09@hotmail.com>
2020-04-20 15:02:39 +0700

    slot

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
+var food = {
+    template:'#foodarea'
+};
 
-Vue.component('showcomment',{
-    props:['comment'],
-    template:'<h3 style="color: #F44336">{{comment}}</h3>'
-})
 new Vue({
     el:"#show_post",
-    data:{
-        newComment:'',
-        comment:[
-            'a','b','c'
-        ]
-    },
-    methods:{
-        addComment:function(){
-            this.comment.push(this.newComment)
-            this.newComment = ''
-        }
-    }
+   components:{
+       foodApp:food
+   }
 });
