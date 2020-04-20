f8d84618000765686a5066097abd02e523346338
For_fluke <flukeza09@hotmail.com>
2020-04-20 14:07:10 +0700

    add value in component

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ Vue.component('tawin',{
     },
     template:'<button v-on:click="count++">click : {{count}}</button>'
 });
+
+Vue.component('post',{
+    props:['title'],
+    template:'<h3>{{title}}</h3>'
+});
 new Vue({
     el:"#show_post"
 });
